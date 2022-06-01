d733c5e7017cc785736c8ca55426aaf8851bf441
Aya Ben Taarit <taarit.aya@gmail.com>
2022-06-01 12:25:25 +0200

    Set properties using properties source, env variables or command line args.

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,15 +1,27 @@
 package guru.springframework.sfgdi.config;
 
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import pets.PetService;
 import pets.PetServiceFactory;
-
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:di-config.xml")
 @Configuration
 public class GreetingServiceConfig {
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}") String password,
+                                  @Value("${guru.jdbcurl}") String jdbcurl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+        return fakeDataSource;
+    }
 
     @Bean
     PetServiceFactory petServiceFactory(){
